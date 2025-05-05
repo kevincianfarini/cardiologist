b36a20635e905ebce4b361e6736f45a03649d152
Kevin Cianfarini <kevincianfarini@gmail.com>
2025-05-05 10:43:10 -0400

    Remove Clock.delayFor and Clock.delayUntilNext

--- src/commonMain/kotlin/io/github/kevincianfarini/cardiologist/suspend.kt
+++ src/commonMain/kotlin/io/github/kevincianfarini/cardiologist/suspend.kt
@@ -3,21 +3,16 @@ package io.github.kevincianfarini.cardiologist
 import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.delay
 import kotlinx.datetime.Clock
-import kotlinx.datetime.DateTimePeriod
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.atTime
-import kotlinx.datetime.plus
 import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
 
 /**
  * Suspend the coroutine until [instant] without blocking the thread. If [Clock.now] is greater than [instant], this
  * function returns immediately.
  *
- * Coroutines delayed for more than one minute will occassionally be resumed to account for clock drift.
+ * Coroutines delayed for more than one minute will occasionally be resumed to account for clock drift.
  */
 public suspend fun Clock.delayUntil(instant: Instant) {
     var now = now()
@@ -33,35 +28,8 @@ public suspend fun Clock.delayUntil(instant: Instant) {
  * Suspend the coroutine until [dateTime] in [timeZone] without blocking the thread. If [Clock.now] is greater than
  * the [Instant] associated with [dateTime] and [timeZone], this function returns immediately.
  *
- * Coroutines delayed for more than one minute will occassionally be resumed to account for clock drift.
+ * Coroutines delayed for more than one minute will occasionally be resumed to account for clock drift.
  */
 public suspend fun Clock.delayUntil(dateTime: LocalDateTime, timeZone: TimeZone) {
     delayUntil(instant = dateTime.toInstant(timeZone))
-}
-
-/**
- * Suspend the coroutine for [period] in [timeZone] without blocking the thread.
- *
- * Coroutines delayed for more than one minute will occassionally be resumed to account for clock drift.
- */
-public suspend fun Clock.delayFor(period: DateTimePeriod, timeZone: TimeZone) {
-    val now = now()
-    val futureInstant = now.plus(period, timeZone)
-    delayUntil(futureInstant)
-}
-
-/**
- * Suspend the coroutine until the next occurance of [time] in [timeZone] without blocking the thread.
- *
- * Coroutines delayed for more than one minute will occassionally be resumed to account for clock drift.
- */
-public suspend fun Clock.delayUntilNext(time: LocalTime, timeZone: TimeZone) {
-    val now = now()
-    val nowLocal = now.toLocalDateTime(timeZone)
-    val desiredLocal = nowLocal.date.atTime(time)
-    val futureInstant = when {
-        desiredLocal < nowLocal -> desiredLocal.toInstant(timeZone).plus(DateTimePeriod(days = 1), timeZone)
-        else -> desiredLocal.toInstant(timeZone)
-    }
-    delayUntil(futureInstant)
 }
