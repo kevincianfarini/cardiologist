8c800880bd58db5de2e7b5b60b34b7cca248d2de
Kevin Cianfarini <kevincianfarini@gmail.com>
2025-05-10 15:18:08 -0400

    Stop modeling PulseBackpressureStrategy as an Enum (#113)
    Closes #110

--- src/commonMain/kotlin/io/github/kevincianfarini/cardiologist/PulseBackpressureStrategy.kt
+++ src/commonMain/kotlin/io/github/kevincianfarini/cardiologist/PulseBackpressureStrategy.kt
@@ -1,22 +1,26 @@
 package io.github.kevincianfarini.cardiologist
 
-public enum class PulseBackpressureStrategy {
+import kotlin.jvm.JvmInline
 
-    /**
-     * Schedules recurring jobs concurrently. That is if job `n` is still active when job
-     * `n + 1` should begin, then job `n` continues to run concurrently alongside job `n + 1`.
-     */
-    ExecuteConcurrently,
+@JvmInline
+public value class PulseBackpressureStrategy private constructor(private val value: Byte) {
+    public companion object {
+        /**
+         * Schedules recurring jobs concurrently. That is if job `n` is still active when job
+         * `n + 1` should begin, then job `n` continues to run concurrently alongside job `n + 1`.
+         */
+        public val ExecuteConcurrently: PulseBackpressureStrategy get() = PulseBackpressureStrategy(0)
 
-    /**
-     * Schedules recurring jobs sequentially by cancelling an unfinished job if a new one
-     * should begin executing.
-     */
-    CancelPrevious,
+        /**
+         * Schedules recurring jobs sequentially by cancelling an unfinished job if a new one
+         * should begin executing.
+         */
+        public val CancelPrevious: PulseBackpressureStrategy get() = PulseBackpressureStrategy(1)
 
-    /**
-     * Schedules recurring jobs sequentially by skipping a new job if an unfinished job
-     * is still executing.
-     */
-    SkipNext,
+        /**
+         * Schedules recurring jobs sequentially by skipping a new job if an unfinished job
+         * is still executing.
+         */
+        public val SkipNext: PulseBackpressureStrategy get() = PulseBackpressureStrategy(2)
+    }
 }
